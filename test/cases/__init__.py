a1716e28c679833d3e730638f4d6259aebfdfd14
Joshua Griffin Dunn <joshua.g.dunn@gmail.com>
2015-07-07 10:46:06 -0700

    Conversion from independent `argdoc` package to part of `sphinxcontrib` namespace package as `sphinxcontrib.argdoc` Required lots of file movement and renaming. Hopefully this won't hose their history

--- test/cases/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-"""Modules in this package are executable scripts that serve as test cases
-for :mod:`argdoc.test.test_ext`. They cover a variety of :mod:`argparse`
-configurations.
-"""
